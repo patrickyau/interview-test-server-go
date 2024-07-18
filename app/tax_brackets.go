00fd7679f6e3e827ca5f892ce36041329dbfdeda
Patrick Yau <40751171+patrickyau@users.noreply.github.com>
2024-07-18 17:21:46 -0400

    Update Presentation file

--- app/tax_brackets.go
+++ app/tax_brackets.go
@@ -1,7 +1,6 @@
 package main
 
 // TaxBracket represents a tax bracket for a given year.
-// This allows the Decorator design pattern to be used to expand with other tax bracket criteria.
 type TaxBracket struct {
 	Min     float64 `json:"min"`
 	Max     float64 `json:"max"`
@@ -10,7 +9,7 @@ type TaxBracket struct {
 }
 
 // TaxBrackets represents the tax brackets for all supported years.
-// A Singleton design pattern is used to ensure that only one instance of the TaxBrackets map is created.
+// Use Singleton pattern
 var TaxBrackets = map[string][]TaxBracket{
 	"2019": {
 		{Min: 0,
